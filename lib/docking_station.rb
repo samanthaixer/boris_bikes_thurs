95448c2121008f0f391f512efc3d00e141c60915
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-07 17:21:38 +0000

    Docking station takes 20 bikes (14)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,22 +2,21 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail 'Cannot release a bike - no bikes docked' unless @bike
-    @bike
-
-    # if @bike.nil?
-    #   raise "Cannot release a bike - no bikes docked"
-    # else
-    #   @bike
-    # end
+    fail 'No bikes available' unless @bikes.count > 0
+    @bikes.pop
   end
 
   def dock(our_bike)
-    fail "Cannot dock bike - there is already a bike" if @bike
-    @bike = our_bike
+    fail "Docking station full" if @bikes.count == 20
+    @bikes.push(our_bike)
+    @bikes
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,18 +1,25 @@
 require 'docking_station'
 describe DockingStation do
 
+  describe '#initialize' do
+
+    it 'creates an array' do
+      expect(subject.bikes).to eq []
+    end
+
+  end
+
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
 
     it 'gets a bike and the bike is working' do
-      my_bike = Bike.new
-      subject.dock(my_bike)
+      subject.dock(Bike.new)
       bike = subject.release_bike
       expect(bike).to be_working
     end
 
     it 'raise an exception if no bike is available to release' do
-      expect{ subject.release_bike }.to raise_error("Cannot release a bike - no bikes docked")
+      expect{ subject.release_bike }.to raise_error("No bikes available")
     end
   end
 
@@ -21,21 +28,19 @@ describe DockingStation do
 
     it 'shows the bike that has been docked' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to include bike
     end
 
     it 'raise an exception if there is already a bike in the docking station' do
-      my_bike = Bike.new
-     subject.dock(my_bike)
-     other_bike = Bike.new
-      expect{ subject.dock(other_bike) }.to raise_error("Cannot dock bike - there is already a bike")
+      20.times { subject.dock Bike.new }
+      expect{ subject.dock(Bike.new) }.to raise_error("Docking station full")
     end
 
 
     it 'show a bike has been docked' do
       my_bike = Bike.new
       subject.dock(my_bike)
-      expect(subject.bike).to eq my_bike
+      expect(subject.bikes).to include my_bike
     end
   end
 end
