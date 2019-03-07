fa0bda21f3016807800376b0d329975f0793ad6f
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-07 15:22:05 +0000

    Created dock(our_bike) method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
-require 'bike'
+# require_relative 'bike'
+
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
-    Bike.new 
+    Bike.new
   end
+
+  def dock(our_bike)
+    @bike = our_bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,24 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
+
   it 'gets a bike and the bike is working' do
 
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'shows the bike that has been docked' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'show a bike has been docked' do
+    my_bike = Bike.new
+    subject.dock(my_bike)
+    expect(subject.bike).to eq my_bike
   end
 
 end
